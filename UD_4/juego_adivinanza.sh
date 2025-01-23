87d0e9465624e8834bc288df04d74ca3ea2fa0a8
Victoria-STEM <victoriasegovia001.alu@stemgranada.com>
2025-01-23 11:44:15 +0100

    juego adivinanzo modificado

--- UD_4/juego_adivinanza.sh
+++ UD_4/juego_adivinanza.sh
@@ -9,42 +9,62 @@ reglas_juego() {
     echo "3. El juego finaliza cuando aciertas el número o decides salir."
 }
 
-reglas_juego
+volver_jugar() {
 
-numero="$((RANDOM % 101))"
-juegoTerminado=false
-contador=0
+    read -p "Volver a jugar? (1) Si, (2) No." usuario
 
-while [ "$juegoTerminado" = false ]; do
-
-    read -p "Introduce un numero: " usuario
-
-    if [ "$usuario" = "salir" ]; then
-        echo "Juego terminado."
+    if  [ "$usuario" -eq 2 ]; then
         juegoTerminado=true
 
+    elif [ "$usuario" -eq 1 ]; then 
+        jugar_adivinanza
+
     else
+        echo "Introduce un valor correcto."
+    fi
 
-        if [ "$usuario" -ge 0 ] && [ "$usuario" -le 100 ]; then
+}
 
-            contador=$((contador + 1))
+jugar_adivinanza() {
 
-            if [ "$usuario" -lt "$numero" ]; then
-                echo "El numero es mayor que $usuario."
+    numero="$((RANDOM % 101))"
+    juegoTerminado=false
+    contador=0
+    volverJugar=0
 
-            elif [ "$usuario" -gt "$numero" ]; then
-                echo "El numero es menor que $usuario."
-
-            else
-                echo "Felicidades has acertado el $numero en $contador intentos."
-                juegoTerminado=true
-
-            fi
-
+    while [ "$juegoTerminado" = false ]; do
+    
+        read -p "Introduce un numero: " usuario
+    
+        if [ "$usuario" = "salir" ]; then
+            volver_jugar
+    
         else
-            echo "Introduce un numero entre 1 y 100 correcto."
-        fi
+    
+            if [ "$usuario" -ge 0 ] && [ "$usuario" -le 100 ]; then
+    
+                contador=$((contador + 1))
+    
+                if [ "$usuario" -lt "$numero" ]; then
+                    echo "El numero es mayor que $usuario."
+    
+                elif [ "$usuario" -gt "$numero" ]; then
+                    echo "El numero es menor que $usuario."
+    
+                else
+                    echo "Felicidades has acertado el $numero en $contador intentos."
+                    volver_jugar
+                fi
+    
+            else
+                echo "Introduce un numero entre 1 y 100 correcto."
+            fi
+    
+        fi 
+    
+    done
 
-    fi 
+}
 
-done
+reglas_juego
+jugar_adivinanza
